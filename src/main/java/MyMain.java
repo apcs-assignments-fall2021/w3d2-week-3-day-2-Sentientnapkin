0031e8f260e5bf6aeb2b54c4f62cc34e3165cb25
Sentientnapkin <24svargas@athenian.org>
2021-09-15 14:15:29 -0700

    chars with bytes

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -7,8 +7,7 @@ public class MyMain {
     //     isLetter('?') => false
     //     isLetter('4') => false
     public static boolean isLetter(char c) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        return (c>='A'&&c<='Z'||c>='a'&&c<='z');
     }
 
     // This method is given two chars as input, c1 and c2
@@ -21,7 +20,9 @@ public class MyMain {
     //                             'f'
     //                             'g'
     public static void printChars(char c1, char c2) {
-        // REPLACE WITH YOUR CODE HERE
+        for(int i = c1;i<=c2;i++){
+            System.out.println((char)i);
+        }
     }
 
     // Given a char ch, and returns the next letter in the alphabet.
@@ -34,8 +35,9 @@ public class MyMain {
     //     nextLetter('a') => 'b'
     //     nextLetter('z') => 'a'
     public static char nextLetter(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return ' ';
+        if (ch!='Z'&&ch!='z')
+        return (char)(ch+1);
+        else return (char)(ch-25);
     }
 
     // Similar to the previous example, the method is given a char ch. However, the method
@@ -46,8 +48,9 @@ public class MyMain {
     //     nextNextNextLetter('a') => 'd'
     //     nextNextNextLetter('z') => 'c'
     public static char nextNextNextLetter(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return ' ';
+        if (ch>='A'&&ch<='W'||ch>='a'&&ch<='w')
+        return (char)(ch+3);
+        else return (char)(ch-23);
     }
 
 
